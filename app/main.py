be37dcdcdeec48cb22f22a792ab3c9ae1bcd1eb5
David <davidsalinastec@gmail.com>
2026-02-04 13:25:35 +0100

    Enable CORS and fix frontend API URL

--- app/main.py
+++ app/main.py
@@ -3,10 +3,20 @@ from fastapi import FastAPI
 from .database import engine
 from . import models
 from .routers import tasks
+from fastapi.middleware.cors import CORSMiddleware
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # en producción se restringe
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
